722833013869ae7f079c4f9d66715280450161a5
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-04-12 10:00:41 +0530

    Selector

---
+++ app.js
@@ -0,0 +1,14 @@
+// Jquery Selectors
+
+$(document).ready(() => {
+  // Class Selector
+
+  let box = $(".box1");
+  console.log(box);
+
+  // Id Selector
+
+  let box2 = $("#box2");
+
+  console.log(box2);
+});
