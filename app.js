f981ac972c6a1ee363bc4372d08a26fb1cce65eb
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-04-13 10:13:28 +0530

    Jquery Window Events

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-// ************************************** Jquery Form Events **************************************
+// ************************************** Jquery Window Events **************************************
 
-// (1) focus
-// (2) blur
-// (3) select
-// (4) change
-// (5) submit
+// (1) scroll
+// (2) resize
 
 $(document).ready(() => {
-  // ******************* Focus Form Event *******************
+  // ******************* Scroll Form Event *******************
 
-  $("input[type=email] , input[type=password]").focus(function () {
-    $(this).css("background-color", "#eee");
+  $(window).scroll((e) => {
+    console.log(window.scrollY);
   });
 
-  // ******************* Blur Form Event *******************
+  // ******************* Resize Form Event *******************
 
-  $("input[type=email] , input[type=password]").blur(function () {
-    $(this).css("background-color", "#fff");
-  });
-
-  // ******************* Select Form Event *******************
-
-  $("input[type=email] , input[type=password]").select(function () {
-    $(this).css("background-color", "#e8e8e8e8");
-  });
-
-  // ******************* Change Form Event *******************
-
-  $("input[type=email] , input[type=password]").change(function () {
-    console.log(this.value);
-  });
-
-  // ******************* Submit Form Event *******************
-
-  $("#form").submit((e) => {
-    e.preventDefault();
+  $(window).resize(function (e) {
+    $("h1").html(window.outerWidth);
   });
 });
